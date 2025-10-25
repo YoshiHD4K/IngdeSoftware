c8ffe0c8c18c7ab2479865a84d45e6d228ccde8b
Tomas Reveron <tomyreveroncito@gmail.com>
2025-10-25 14:14:49 -0400

    EJercicio:
    - Ingresa un numero y verifica si es palindromo o no

---
+++ ejercicios faciles/palindromesnumericos.py
@@ -0,0 +1,17 @@
+def es_palindromo(numero):
+    str_num = str(numero)
+    return str_num == str_num[::-1]
+
+while True:
+    try:
+        numero = int(input("Ingrese un numero entre 1 y 99999: "))
+        if 1 <= numero <= 99999:
+            if es_palindromo(numero):
+                print(f"El numero {numero} es un palindromo.")
+            else:
+                print(f"El numero {numero} no es un palindromo.")
+            break
+        else:
+            print("El numero debe estar entre 1 y 99999. Intente de nuevo.")
+    except ValueError:
+        print("Entrada invalida. Por favor ingrese un numero entero.")
